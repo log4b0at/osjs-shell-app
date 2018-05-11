7a772c627246edfd020cf39dc38612ba6f882b83
Leo <alpiniste96@gmail.com>
2018-05-11 22:25:06 +0200

    Update

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
 import ShellCore from 'osjs-shell-core';
+import { EventEmitter } from 'events';
 
-export default class ShellApp {
-	
-	constructor(appname){
+export default class ShellApp extends EventEmitter {
+
+	constructor(appname) {
+		super();
 		this.appname = appname;
-		if(!this.appname) {
+		if (!this.appname) {
 			throw new Error("A ShellApp need to be construct with a name.\nEx: new ShellApp('myShellApp') ");
 		}
 	}
-	
-	start(){
-		
+
+	start() {
 		let build = this;
-		
-		OSjs.make('osjs/packages').register(this.appname, (core, args, options, metadata) => {
+
+		OSjs.make('osjs/packages').register(build.appname, (core, args, options, metadata) => {
 			build.process = core.make('osjs/application', {
 				args,
 				options,
 				metadata
 			});
-			
-			this.createWindow(core);
-			
+
+			build.createWindow(core);
+			build.emit('started');
+
 			return build.process;
 		});
 	}
-	
-	createWindow(core){
+
+	createWindow(core) {
 		this.process.createWindow({
-			id: 'ShellWindow_'+this.appname.replace(/\s/g, '_'),
-			title: 'Shell - '+this.appname,
-			dimension: {width: 550, height: 400},
-			position: {left: 300, top: 200}
+			id: 'ShellWindow_' + this.appname.replace(/\s/g, '_'),
+			title: 'Shell - ' + this.appname,
+			dimension: { width: 550, height: 400 },
+			position: { left: 300, top: 200 }
 		})
-		.on('destroy', () => this.process.destroy())
-		.render((container, windowInstance) => this.createShellCore(core, windowInstance, container));
+			.on('destroy', () => this.process.destroy())
+			.render((container, windowInstance) => {
+				this.window = windowInstance;
+				this.createShellCore(core, windowInstance, container);
+				this.emit('done');
+			});
 	}
-	
-	createShellCore(appCore, windowInstance, container){
+
+	createShellCore(appCore, windowInstance, container) {
 		this.core = new ShellCore();
 		container.appendChild(this.core.element);
 	}
