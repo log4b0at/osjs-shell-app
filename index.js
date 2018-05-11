c1ad6ab6fa3a907b7ad6ec495467ea75dabf9772
Leo <alpiniste96@gmail.com>
2018-05-11 21:26:19 +0200

    Debug

--- index.js
+++ index.js
@@ -19,21 +19,22 @@ export default class ShellApp {
 				options,
 				metadata
 			});
+			
+			this.createWindow(core);
+			
+			return build.process;
 		});
-		
-		this.createWindow(core);
 	}
 	
 	createWindow(core){
 		this.process.createWindow({
-			id: 'ShellWindow_'+this.appname,
-			title: metadata.title.en_EN,
+			id: 'ShellWindow_'+this.appname.replace(/\s/g, '_'),
+			title: 'Shell - '+this.appname,
 			dimension: {width: 550, height: 400},
 			position: {left: 300, top: 200}
-		});
+		})
 		.on('destroy', () => this.process.destroy())
 		.render((container, windowInstance) => this.createShellCore(core, windowInstance, container));
-		;
 	}
 	
 	createShellCore(appCore, windowInstance, container){
